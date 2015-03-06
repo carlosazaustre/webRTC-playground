14b985cf04227efde3fff8a973c088b4bd28b4e7
Carlos Azaustre <cazaustre@gmail.com>
2015-03-06 23:55:12 +0100

    add gulpfile and tasks

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// -- Dependencies -------------------------------------------------------------
+
+var gulp = require('gulp');
+var gutil = require("gulp-util");
+var stylus = require("gulp-stylus");
+var nodemon = require("gulp-nodemon");
+var concat = require("gulp-concat");
+var autoprefixer = require("gulp-autoprefixer");
+var header = require("gulp-header");
+var pkg = require("./package.json");
+
+// -- Files --------------------------------------------------------------------
+
+var assets = "/static";
+var source = {
+  styl: [
+    "source/styles/normalize.styl",
+    "source/styles/app.*.styl"
+  ]
+};
+
+var banner = [
+  "/**" +
+  " * <%= pkg.name %>" +
+  " * @version v<%= pkg.version %>" +
+  " */" +
+  ""
+].join("\n");
+
+// -- Tasks --------------------------------------------------------------------
+
+gulp.task("styl", function() {
+  gulp.src(source.styl)
+    .pipe(concat(pkg.name + ".styl"))
+    .pipe(stylus({ compress: true, errors: true }))
+    .pipe(autoprefixer())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest(assets + "/css"));
+  return;
+});
+
+gulp.task("watch", function() {
+  gulp.watch(source.styl, [ "styl" ]);
+  return;
+});
+
+gulp.task("build", function() {
+  gulp.start([ "styl" ]);
+  return;
+});
+
+gulp.task("server", function() {
+  nodemon({
+    script: "index.js",
+    ext: "js",
+    ignore: ".git"
+  });
+});
+
+// -- Runs ---------------------------------------------------------------------
+
+gulp.task("default", function() {
+  gulp.start([ "build", "watch", "server" ]);
+  return;
+});
